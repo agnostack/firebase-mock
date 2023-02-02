c4f25515faebc2917daef182f8ef5734a9d90266
Adam Grohs <adam@uniquelyparticular.com>
2023-02-01 22:14:33 -0500

    feat: added build gulp

--- gulpfile.js
+++ gulpfile.js
@@ -120,7 +120,7 @@ gulp.task('lint', function () {
     .pipe(plugins.jshint.reporter('fail'));
 });
 
-var pkgs = ['./package-lock.json', './package.json', './bower.json'];
+var pkgs = ['./yarn.lock','./package-lock.json', './package.json', './bower.json'];
 gulp.task('bump', function () {
   return gulp.src(pkgs)
     .pipe(plugins.bump({
